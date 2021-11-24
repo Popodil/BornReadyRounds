60a10063b8c8ae54877f03a71ddae4e56b9c3d2b
Yoeri Smits <457510@student.fontys.nl>
2021-11-24 20:01:50 +0100

    - Initial Commit

---
+++ BornReadyCards/BornReadyCards/BornReadyCards.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using UnboundLib.Cards;
+using BornReadyCards.Cards;
+using HarmonyLib;
+
+namespace BornReadyCards
+{
+    [BepInPlugin(ModId, ModName, Version)]
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInProcess("Rounds.exe")]
+    public class BornReadyCards : BaseUnityPlugin
+    {
+        private const string ModId = "com.popodil.rounds.BornReadyCards";
+        private const string ModName = "BornReadyCards";
+        public const string Version = "0.0.1";
+        public const string ModInitials = "BRC";
+
+        public static BornReadyCards instance { get; private set; }
+
+        void Awake()
+        {
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+
+        void Start()
+        {
+            instance = this;
+            CustomCard.BuildCard<CrazySphere>();
+        }
+    }
+}
